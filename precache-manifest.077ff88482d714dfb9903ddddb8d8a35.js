3305655680748749bed4e7908ae1196da206264b
Gustavo Velasquez <gvelasquez@consultoriaglobal.com.ar>
2020-10-27 09:57:15 -0300

    Updates

---
+++ precache-manifest.077ff88482d714dfb9903ddddb8d8a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c267df8abcff92adf04b0e23cd59366",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "278b9a0b99e1a87c0c00",
+    "url": "/drum-machine/static/css/main.2a4c6de7.chunk.css"
+  },
+  {
+    "revision": "b17645b5ce4fe5e8ecbf",
+    "url": "/drum-machine/static/js/2.ed27fbf1.chunk.js"
+  },
+  {
+    "revision": "60cea085f906716cc49a413ef21570f4",
+    "url": "/drum-machine/static/js/2.ed27fbf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "278b9a0b99e1a87c0c00",
+    "url": "/drum-machine/static/js/main.adcd266a.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
